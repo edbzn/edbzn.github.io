9e8687132e6918ffab732d10f6d0d16f9ef092b6
Edouard Bozon <bozonedouard@gmail.com>
2024-11-18 08:01:33 +0100

    Revert "rm -rf *"
    This reverts commit 2616d5e8c10545ae76fa6ff158cb16f010c9b520.

---
+++ README.md
@@ -0,0 +1,5 @@
+# www.codamit.dev
+
+![Codamit](https://media.giphy.com/media/3o6Mb5ZXk5gzKggqBy/giphy.gif)
+
+[Browse to the site](https://www.codamit.dev)
---
+++ gatsby-browser.js
@@ -0,0 +1,9 @@
+import 'typeface-merriweather';
+import 'typeface-montserrat';
+
+import './static/css/code-theme.css';
+import './static/css/main.css';
+
+export const onServiceWorkerUpdateReady = () => {
+  window.location.reload();
+};
